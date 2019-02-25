107710395698cbeba2c8cb3326e0d42be4547829
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-25 16:54:02 -0500

    MovieList cards are now Links

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -31,24 +32,31 @@ export default class MovieList extends Component {
   }
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+const linkStyle = {
+  textDecoration: 'none',
+  color: 'black'
+};
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+function MovieDetails({ movie }) {
+  const { title, director, metascore, stars, id } = movie;
+  return (
+    <Link to={`/movies/${id}`} style={linkStyle}>
+      <div className="movie-card" >
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+    </Link>
   );
 }
