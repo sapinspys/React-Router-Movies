2dc8328e26defc1fb3b3ecb0fdf88706b9b9ab2d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-25 17:06:44 -0500

    Moved movie card code to MovieCard component and refactored MovieList

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const removeLinkStyle = {
   textDecoration: 'none',
@@ -38,25 +39,10 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
+  const { id } = movie;
   return (
     <Link to={`/movies/${id}`} style={removeLinkStyle}>
-      <div className="movie-card" >
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
     </Link>
   );
 }
