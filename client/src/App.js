4e724daf35569a11378d2e389abe65835bbbb745
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-26 13:06:14 -0500

    Converted saved movi Links to NavLinks, inline style already in place

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <Route path='/' exact component={MovieList} />
         {/* <Route path='/movies/:id' component={Movie} /> */}
-        <Route path='/movies/:id' render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />} />
+        <Route path='/movies/:id' render={props => <Movie {...props} addToSavedList={this.addToSavedList} />} />
       </div>
     );
   }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import removeLinkStyle from './ReusableStyles';
 
 export default class SavedList extends Component {
@@ -12,12 +12,11 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`} 
+          <NavLink to={`/movies/${movie.id}`} 
             key={movie.id}
-            className="saved-movie"
             style={removeLinkStyle}>
               {movie.title}
-          </Link>
+          </NavLink>
         ))}
         <Link to='/' style={removeLinkStyle}>
           <div className="home-button">Home</div>
