27313c070e1b47c15708d661bfe619313083519c
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-25 20:09:16 -0500

    Passed addToSavedList function down to Movie component and added save functionality

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,8 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
         <Route path='/' exact component={MovieList} />
-        <Route path='/movies/:id' component={Movie} />
+        {/* <Route path='/movies/:id' component={Movie} /> */}
+        <Route path='/movies/:id' render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />} />
       </div>
     );
   }
